8e27312d6a533aa03c5aa900f65b19dc678cd531
tonybatty <tonyeb87@gmail.com>
2020-06-10 22:27:14 +0100

    add hue buttons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,16 +2,14 @@ import React, { Component } from 'react';
 import './Home.css';
 
 const randomColor = require('randomcolor')
-var color = ''
-
-console.log("!", color)
 
 export default class Home extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      color: '#eeeeee'
+      color: '#eeeeee',
+      hue: ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'monochrome']
     }
 
     this.handleClick = this.handleClick.bind(this)
@@ -30,7 +28,14 @@ export default class Home extends Component {
   render() {
     return (
       <div className="container" style={{ backgroundColor: this.state.color }} onClick={this.handleClick}>
+        
         <div className="hex-code">{this.state.color}</div>
+
+        <div className="hue-button-group">
+          {this.state.hue.map((item, index) => (
+            <div className="hue-button" style={{ backgroundColor: item }}></div>
+          ))}
+        </div>
       </div>
     )
   }
