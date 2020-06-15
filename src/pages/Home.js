9aec2fabc958fef842920974b73cd08561b0ad5a
tonybatty <tonyeb87@gmail.com>
2020-06-15 22:09:54 +0100

    add function to get contrasting text color from background color

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -66,7 +66,6 @@ export default class Home extends Component {
 
   clickHue(e, hue) {
     e.stopPropagation()
-    console.log("click hue")
     if (this.state.selectedHue !== hue) {
       this.setState({
         hue,
@@ -80,9 +79,9 @@ export default class Home extends Component {
 
   getTextColor(bgColor, lightColor, darkColor) {
     var color = bgColor.charAt(0) === "#" ? bgColor.substring(1, 7) : bgColor
-    var r = parseInt(color.substring(0, 2), 16) // hexToR
-    var g = parseInt(color.substring(2, 4), 16) // hexToG
-    var b = parseInt(color.substring(4, 6), 16) // hexToB
+    var r = parseInt(color.substring(0, 2), 16)
+    var g = parseInt(color.substring(2, 4), 16)
+    var b = parseInt(color.substring(4, 6), 16)
     return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? darkColor : lightColor
   }
 
