392de4fdf0a824f6dce4fdb7880d66077401223e
tonybatty <tonyeb87@gmail.com>
2020-08-15 14:48:01 +0100

    remove console log

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -87,7 +87,6 @@ export default class Home extends Component {
     var r = parseInt(color.substring(0, 2), 16)
     var g = parseInt(color.substring(2, 4), 16)
     var b = parseInt(color.substring(4, 6), 16)
-    console.log(r * 0.299 + g * 0.587 + b * 0.114)
     return r * 0.299 + g * 0.587 + b * 0.114 > 120
   }
 
